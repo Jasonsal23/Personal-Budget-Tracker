d654a45fc9309cb87650d1960703ec55a5bbbe1e
Jason <jason@Jasons-MacBook-Air.local>
2023-03-07 18:42:23 -0800

    code

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+window.prompt (weeklyExpenseQuestions[0])
+console.log (answer)
+let StringAnswer = window.prompt(weeklyEspenseQuestions[0])
+let numberAnswer = parseFloat (stringAnswer)
+console.log (numberAnswer)
